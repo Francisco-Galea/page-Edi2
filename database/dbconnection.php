09f92999dab1627ed6f7e971b186d68db52a8ebb
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-10 15:26:17 -0300

    fixed issue

--- database/dbconnection.php
+++ database/dbconnection.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "edi2";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
