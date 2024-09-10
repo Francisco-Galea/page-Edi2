f8cbd69a1c565d65d75782afea168c82a98eb907
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-10 15:28:26 -0300

    Added new table

--- database/edi2.sql
+++ database/edi2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-08-2024 a las 04:10:25
+-- Tiempo de generación: 10-09-2024 a las 20:27:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `guides`
+--
+
+CREATE TABLE `guides` (
+  `id` int(11) NOT NULL,
+  `video_url` varchar(255) NOT NULL,
+  `thumbnail_url` varchar(255) GENERATED ALWAYS AS (concat('https://img.youtube.com/vi/',substring_index(substring_index(`video_url`,'v=',-1),'&',1),'/maxresdefault.jpg')) VIRTUAL,
+  `author` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `guides`
+--
+
+INSERT INTO `guides` (`id`, `video_url`, `author`, `description`, `created_at`) VALUES
+(2, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'Rick Astley', 'El video oficial de \"Never Gonna Give You Up\".', '2024-09-10 18:07:42'),
+(3, 'https://www.youtube.com/watch?v=ehMGRV-OXUQ', 'Franga', 'Genichisto no hit lvl 1\".', '2024-09-10 18:10:09');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user`
 --
 
@@ -38,12 +61,17 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userid`, `username`, `password`) VALUES
-(1, 'fran', '202cb962ac59075b964b07152d234b70'),
-(2, 'admin', '202cb962ac59075b964b07152d234b70');
+(3, 'fran', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `guides`
+--
+ALTER TABLE `guides`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `user`
@@ -57,10 +85,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `guides`
+--
+ALTER TABLE `guides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
