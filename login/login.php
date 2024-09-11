3f52efdbb3efcfbf467143df52ea8f52d2071c6b
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-11 20:48:47 -0300

    Added logout

--- login/login.php
+++ login/login.php
@@ -1,16 +1,16 @@
 <?php
 require_once('../database/dbConnection.php');
+session_start();
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = md5($_POST['password']);
-
     $query = $conn->query("SELECT * FROM user WHERE username='$username' AND password='$password'");
 
     if ($query->num_rows > 0) {
         $row = $query->fetch_array();
         $_SESSION['user'] = (int)$row['userid'];
-        echo "<span>WASAAA</span>";
+        header("Location: ../dashboard.php");
         exit();
     } else {
         echo "<span>Login fallido. Usuario no encontrado.</span>";
